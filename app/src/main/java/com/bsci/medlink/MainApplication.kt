23debe8029d9f284edff9f0596edb4bba6c24e9a
xuf <123456>
2025-12-30 16:15:49 +0800

    add the feature of agora meeting and add host registration logic and client management.

--- app/src/main/java/com/bsci/medlink/MainApplication.kt
+++ app/src/main/java/com/bsci/medlink/MainApplication.kt
@@ -2,9 +2,13 @@ package com.bsci.medlink
 
 import android.app.Application
 import com.bsci.medlink.common.model.GlobalSettings
+import com.bsci.medlink.data.model.Client
 
 class MainApplication: Application() {
     var globalSettings: GlobalSettings = GlobalSettings()
+    
+    // 缓存客户端列表，在 SplashActivity 中预加载
+    var cachedClients: List<Client>? = null
 
     override fun onCreate() {
         super.onCreate()
